c95e403316898d38acd61578062cbfb012b97c80
Paola <camachopaola311@gmail.com>
2020-03-01 18:40:33 -0500

    Finished staying logged in, logging out, and adding comments

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -12,6 +12,7 @@ import Parse
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -23,9 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "http://parst.herokuapp.com/parse"
             })
         )
-        
         return true
     }
+    
 
     // MARK: UISceneSession Lifecycle
 
